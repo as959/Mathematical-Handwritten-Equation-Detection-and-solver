a3083d07b5de99deeb8930d7c07097c543c8fd96
as959 <46996519+as959@users.noreply.github.com>
2020-05-11 12:47:10 +0530

    Update Equation_GUI.py

--- Equation_GUI.py
+++ Equation_GUI.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import canvas as can
+import paint_app as can
 import segmentation_expo_position as segexp
 import Model_Detection as dmodel
 import plot
@@ -9,10 +9,10 @@ def solvefun():
        segexp.segstart()                           # Segment the image obtained after drawing and savein current directory
        equation,type, roots = str(dmodel.detect())          ### Call function detect from Detection_Model python file in same directory ###
 
-       w = Label(root, text=type, height=1, font='Arial', justify='right', pady=1, bg='#AEE7FB')
-       w.grid(row=6, column=0)
-       w = Label(root, text=roots, height=1, font='Arial', bg='#AEE7FB')
-       w.grid(row=8, column=0)
+       w = Label(root, text=type, font=("times new roman", 16, "bold"), pady=1, bg='#AEE7FB')
+       w.place(x=130, y=378, height=50, width=370)
+       w = Label(root, text=roots, font=("times new roman", 16, "bold"), pady=1, bg='#AEE7FB')
+       w.place(x=130, y=478, height=50, width=370)
 
 
 def graph():
@@ -28,41 +28,28 @@ if __name__ == "__main__":
    root.title("Equalogy")
 
    # set the configuration of GUI window
-   root.geometry("490x390")
+   root.geometry("600x700")
    C = Canvas(root, bg="blue", height=250, width=300)
-   filename = PhotoImage(file="bgfinal1.png")
+   filename = PhotoImage(file="equation_bg.png")
    background_label = Label(root, image=filename)
    background_label.place(x=0, y=0, relwidth=1, relheight=1)
-   w = Label(root, text="      Mathematical  Equation  Detection  And  Solver        ",height=2,font='bold' ,padx=1,pady=1,bg='#AEE7FB')
-   w.grid(row=0,column=0)
-   w = Label(root, text="Draw the Equation on Canvas ", height=1, font='Arial', justify='right', pady=1,bg='white')
-   w.grid(row=1,column=0)
-   button1 = Button(root, text='  Click to Draw  ', fg='black', bg='#AEE7FB',font='System',
-                    command=can.canvasbeg, height=2, width=16)          ### call the function vanvasbeg from paint_app.py ###
-   button1.grid(row=2, column=0)
-   w = Label(root, text="Press Solve to get Solution", height=1, font='Arial', justify='right', pady=1, bg='white')
-   w.grid(row=3, column=0)
-   button2 = Button(root, text=' Solve ', fg='black', bg='#AEE7FB',font='System',
+   w = Label(root, text="      Mathematical  Equation  Detection  And  Solver        ",font=("times new roman",18, "bold") ,padx=1,pady=1,bg='#AEE7FB')
+   w.place(x=0,y=0,height=70,width=600)
+
+   button1 = Button(root, text='  Draw your Equation  ', fg='black', bg='#AEE7FB',font=("times new roman",16, "bold"),command=can.canvasbeg)          ### call the function vanvasbeg from paint_app.py ###
+   button1.place(x=140,y=90,height=90,width=350)
+
+   button2 = Button(root, text=' Get solution for equation ', fg='black', bg='#AEE7FB',font=("times new roman",16, "bold"),
+                    command=solvefun)
+   button2.place(x=140,y=230,height=90,width=350)
+   w = LabelFrame(root, text="Type of Equation ", height=1, font=("times new roman",16, "bold"), pady=1,bg='#AEE7FB')
+   w.place(x=120,y=350,height=80,width=390)
+   
+   
+   w = LabelFrame(root, text="Roots of Equation are", height=1, font=("times new roman",16, "bold"), pady=1, bg='#AEE7FB')
+   w.place(x=120,y=450,height=80,width=390)
+  
+   button3 = Button(root, text='Get the graph ', fg='black', bg='#AEE7FB', font=("times new roman",16, "bold"),
                     command=solvefun, height=2, width=16)
-   button2.grid(row=4, column=0)
-   w = Label(root, text="Type of Equation ", height=1, font='Arial', justify='right', pady=1,bg='white')
-   w.grid(row=5, column=0)
-   equation1 = StringVar()
-   w = Label(root, text="              ", height=1, font='Arial', justify='right', pady=1, bg='#AEE7FB')
-   w.grid(row=6, column=0)
-
-   w = Label(root, text="Roots of Equation are ", height=1, font='Arial', bg='white')
-   w.grid(row=7, column=0)
-
-   equation2 = StringVar()
-
-   w = Label(root, text="            ", height=1, font='Arial', bg='#AEE7FB')
-   w.grid(row=8, column=0)
-   w = Label(root, text="Corresponding Graph  ", height=1, font='Arial', justify='right', pady=1,bg='white')
-   w.grid(row=9, column=0)
-   button3 = Button(root, text=' Graph ', fg='black', bg='#AEE7FB', font='System',
-                    command=graph, height=2, width=16)
-   button3.grid(row=11, column=0)
+   button3.place(x=140,y=580,height=90,width=350)
    root.mainloop()
-
-
